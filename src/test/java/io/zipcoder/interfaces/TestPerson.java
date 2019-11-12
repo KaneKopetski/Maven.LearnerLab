ce941d3103e3036c9932b1076f086fda01164019
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-11-12 16:53:49 -0500

    Complete person and persontest

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,39 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestPerson {
+    Person person;
+    String expectedName = "Kane";
+    Long expectedId = 15L;
+
+    @Before
+    public void testSetup() {
+
+        Person person = new Person(expectedId, expectedName);
+        this.person = person;
+    }
+
+    @Test
+    public void constructorTest() {
+        String actualName = person.getName();
+        Long actualId = person.getId();
+
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void setNameTest() {
+        String expectedName = "New";
+        person.setName(expectedName);
+
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedName, actualName);
+
+    }
 
 }
